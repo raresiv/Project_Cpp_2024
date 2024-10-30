be389f0bafa1a759b9726830ebd5b2725fa46315
Rares <147023885+raresiv@users.noreply.github.com>
2024-10-30 16:54:34 +0100

    modification de la formule du delta (inversion put et call)

--- BlackScholesPricer.cpp
+++ BlackScholesPricer.cpp
@@ -26,7 +26,7 @@ double BlackScholesPricer::delta()
 {
     double d1 = (log(asset_price / option->_strike) + ((interest_rate + pow(volatility, 2) / 2) * option->_expiry)) / (volatility * sqrt(option->_expiry));
     double val_ret = 0;
-    if ((*option).GetOptionType()==optionType::Put)
+    if ((*option).GetOptionType()==optionType::Call)
     {
         val_ret = normalCDF(d1);
     }
