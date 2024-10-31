b295e781a1fbcbc5cf2c13200227f529a131e401
Judith GERVAIS <judith.gervais@edu.devinci.fr>
2024-10-31 12:10:33 +0100

    Rajout des tests de la partie 1

--- Projet1.0.cpp
+++ Projet1.0.cpp
@@ -67,6 +67,7 @@ int main()
     // Calcul du Delta de l'option
     double delta = pricer.delta();
     std::cout << "Delta de l'option Call : " << delta << " (valeur attendue : environ 0.6368)" << std::endl;
+    std::cout << "TEST";
     
     return 0;
 }
