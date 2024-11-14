a3be93030619e581f65f8037f2cda90a4578195e
samuelbontemps0 <samue@ZENBOOK_DE_SAM>
2024-11-14 08:29:32 +0100

    Amélioration get potentielle Sam

--- CRRPricer.cpp
+++ CRRPricer.cpp
@@ -1,13 +1,14 @@
 #include "CRRPricer.h"
 
 
-CRRPricer::CRRPricer(Option* opt, int N, double S, double U, double D, double R) {
+CRRPricer::CRRPricer(Option* opt, int N, double S, double U, double D, double R, BinaryTree<double> arbre) {
 	option = opt;
 	depth = N;
 	asset_price = S;
 	up = U;
 	down = D;
 	interest_rate = R;
+	tree = arbre;
 
 	if (D < R < U) {
 		std::cout << " NO ARBITRAGE";
@@ -18,14 +19,12 @@ CRRPricer::CRRPricer(Option* opt, int N, double S, double U, double D, double R)
 }
 
 BinaryTree<double> CRRPricer::tree_structure() {
-	BinaryTree<double> tree(depth);
-	tree.setNode(0, 0, asset_price);
-	return tree;
+	return tree.setNode(0, 0, asset_price);
 
 }
 
-void CRRPricer::Compute(){
-	BinaryTree<double> tree = tree_structure();
+void CRRPricer::Compute(){	
+
 	for (int i = 0; i < depth; i++) {
 		for (int j = 0; j < depth; j++) {
 			double current_price = asset_price * pow((1 + up), i) * pow(1 + down, depth - i);
@@ -52,4 +51,24 @@ double CRRPricer::get(int n, int i) {
 	return result;
 	
 	
+}
+
+
+double CRRPricer::get(int n, int i) {	//Version Alternative
+	int compteur = 0;
+	double q = (interest_rate - down) / (up - down);
+	double result = option->payoff(asset_price * pow(1 + up, i + 1) * pow(1 + down, depth - i - 1);	//Prix à maturité au noeud i
+	if (compteur == depth - i)
+		return result;
+	else {
+		compteur += 1;	//Compteur augmenté de 1 pour atteindre la limite du dessus (total - i)
+		return (q * get(n + 1, i + 1) + (1 - q) * get(n + 1, i))/(1+interest_rate);	
+		//Formule utilisé car payoff dans la formule est utilisée juste pour maturité pas pour le reste (le reste on utilise les deux prix d'avant)
+	}
+	
+}
+
+double CRRPricer::operator()() {
+	Compute();
+
 }
--- CRRPricer.h
+++ CRRPricer.h
@@ -12,12 +12,13 @@ private:
 	double up;
 	double down;
 	double interest_rate;
+	BinaryTree<double> tree;
 
 public:
-	CRRPricer(Option*, int, double, double, double, double);
+	CRRPricer(Option*, int, double, double, double, double, BinaryTree<double>);
 	BinaryTree<double> tree_structure();
 	void Compute();
 	double get(int, int);
-
+	double operator()();
 };
 
