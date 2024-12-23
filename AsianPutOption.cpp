7b07f4835202fbe1978305957f79e4c248feabf4
LouGirault <114231026+LouGirault@users.noreply.github.com>
2024-12-23 12:45:33 +0100

    ajout_commentaire

--- AsianPutOption.cpp
+++ AsianPutOption.cpp
@@ -1,26 +1,27 @@
 #include "AsianPutOption.h"
 
-double AsianPutOption::payoff(double moy)const
+double AsianPutOption::payoff(double moy)const // calculate the payoff using moy
+// where moy is the mean, calculate into payoffPath into AsianOption
 {
 
-	if (strike > moy)
+	if (strike > moy) // if the strike is greater than the mean
 	{
-		return strike-moy;
+		return strike-moy; // the payoff is equal to the difference
 	}
 	else
 	{
-		return 0;
+		return 0; // 0 else
 	}
 }
 
 optionType AsianPutOption::GetOptionType()const
 {
-	return optionType::Put;
+	return optionType::Put; // in order to get the type : put or call
 }
 
 
 
-AsianPutOption::AsianPutOption(const std::vector<double> vector, double s) : AsianOption(vector, s) {};
+AsianPutOption::AsianPutOption(const std::vector<double> vector, double s) : AsianOption(vector, s) {}; // constructor
 
 //AsianPutOption::AsianPutOption(double value,double s, const std::vector<double> vector) : AsianOption(value,s,vector) {};
 
