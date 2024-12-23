f05d8be97e89cde63f12d2aa6f2bd7f73ccc7fbc
eloi-g <eloi.goutay@edu.devinci.fr>
2024-12-23 22:45:44 +0100

    commentaires AmericanOption

--- AmericanOption.cpp
+++ AmericanOption.cpp
@@ -1,6 +1,7 @@
 #include "AmericanOption.h"
-AmericanOption::AmericanOption(double e, double s) : Option(e)
+AmericanOption::AmericanOption(double e, double s) : Option(e) // constructor initialized with the expiry from option and a strike price given in parameter
 {
+	// checking if the strike price is positive
 	if (s > 0)
 	{
 		_strike = s;
@@ -11,10 +12,10 @@ AmericanOption::AmericanOption(double e, double s) : Option(e)
 		cout << "strike negatif";
 	}
 };
-bool AmericanOption::isAmericanOption() {
-	return true;
+bool AmericanOption::isAmericanOption() { // method helping to consider the option as an American option
+	return true; 
 }
 
-double AmericanOption::GetStrike()const {
+double AmericanOption::GetStrike()const { // getter returning the strike price
 	return _strike;
 }
--- AmericanOption.h
+++ AmericanOption.h
@@ -5,13 +5,13 @@
 #include <vector>
 using namespace std;
 class AmericanOption :
-    public Option
+    public Option // AmericanOption inherits fom Option
 {
 public:
-    AmericanOption(double, double);
-    bool isAmericanOption() override;
-    virtual optionType GetOptionType()const=0;
-    double GetStrike()const;
+    AmericanOption(double, double); // constructor which takes two double values in parameter
+    bool isAmericanOption() override; // method overriding the one from Option class
+    virtual optionType GetOptionType()const=0; // pure virtual method allowing the American option to be a call or put option
+    double GetStrike()const; // getter for the strike value
 protected:
     double _strike;
 };
